a08dc8da3faf9c14af141116da368aeb920645d1
Jacob Causey <cau16002@byui.edu>
2022-04-20 10:24:35 -0600

    project2

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿Console.WriteLine("What was your grade?");
+int grade = Console.Read();
+if (grade >= 90)
+{
+    Console.WriteLine("Your grade is an A!");
+}
+/* else if (grade >= 80)
+{
+    Console.WriteLine("Your grade is a B!");
+}
+else if (grade >= 80)
+{
+    Console.WriteLine("Your grade is a B!");
+}
+else if (grade >= 80)
+{
+    Console.WriteLine("Your grade is a B!");
+}
+else if (grade >= 80)
+{
+    Console.WriteLine("Your grade is a B!");
+}
+*/
