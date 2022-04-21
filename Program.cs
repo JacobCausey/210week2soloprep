ad7125af133def63b4be0940f71e4f8be6f29f5a
Jacob Causey <cau16002@byui.edu>
2022-04-20 19:07:49 -0600

    probably closer now?

--- Program.cs
+++ Program.cs
@@ -1,37 +1,42 @@
-﻿//ask user for input and take a grade to store as a variable.
+﻿using System;
 
-Console.WriteLine("What was your grade?");
-string grade = Console.Read();
-int number = int.Parse(grade);
-Console.WriteLine("$Your grade was a {number}.");
+namespace Program.Console
+{
+    class Program
+    {
+    static void Main(string[] args)
+    {
+        //ask user for input and take a grade to store as a variable/convert from a string to a number.
+        Console.WriteLine("What was your grade?");
+        string response = Console.Read();
+        int grade = int.Parse(response);
+        Console.WriteLine($"Your grade was a {grade}.");
+
 
 //Use an if, else, or else if to convert the int value to a string/grade letter value. (This will be a function)
-{
-    string lettergrade = "";
-    
-    if (number >= 90)
-{
-   string letterGrade ="A";
-   Console.WriteLine($"Your grade is a {letterGrade}");
-}
-else if (number >= 80)
-{
-   string letterGrade ="B";
-   Console.WriteLine("Your grade is a ${letterGrade}");
-}
-else if (number >= 70)
-{
-    string letterGrade ="C";
-    Console.WriteLine("Your grade is a ${letterGrade}");
-}
-else if (number >= 60)
-{
-   string letterGrade ="D";
-   Console.WriteLine("Your grade is a ${letterGrade}");
-}
-else if (number > 60)
-{
-   string letterGrade ="F";
-   Console.WriteLine("Your grade is a ${letterGrade}");
+if (grade >= 90)
+    {
+        string lettergrade = "A";
+    }
+else if (grade >= 80)
+    {
+        string lettergrade = "B";
+    }
+else if (grade >= 70)
+    {
+        string lettergrade = "C";
+    }
+else if (grade >= 60)
+    {
+        string lettergrade = "D";
+    }
+else (grade > 60)
+    {
+        string lettergrade = "F";
+    }
 }
 }
+}
+
+
+        
