a4d2c636bdeb93f6900cba77237599fefee9a3b9
Jacob Causey <cau16002@byui.edu>
2022-04-20 18:38:29 -0600

    howcouldthishappentome

--- Program.cs
+++ Program.cs
@@ -5,31 +5,33 @@ string grade = Console.Read();
 int number = int.Parse(grade);
 Console.WriteLine("$Your grade was a {number}.");
 
-string lettergrade = "";
-
 //Use an if, else, or else if to convert the int value to a string/grade letter value. (This will be a function)
-if (grade >= 90)
+{
+    string lettergrade = "";
+    
+    if (number >= 90)
 {
    string letterGrade ="A";
    Console.WriteLine($"Your grade is a {letterGrade}");
 }
-else if (grade >= 80)
+else if (number >= 80)
 {
    string letterGrade ="B";
    Console.WriteLine("Your grade is a ${letterGrade}");
 }
-else if (grade >= 70)
+else if (number >= 70)
 {
     string letterGrade ="C";
     Console.WriteLine("Your grade is a ${letterGrade}");
 }
-else if (grade >= 60)
+else if (number >= 60)
 {
    string letterGrade ="D";
    Console.WriteLine("Your grade is a ${letterGrade}");
 }
-else if (grade > 60)
+else if (number > 60)
 {
    string letterGrade ="F";
    Console.WriteLine("Your grade is a ${letterGrade}");
+}
 }
